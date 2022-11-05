dec7c75cb57fcf965152a03ccab59c2fc0ba3a50
Kirill <shishkokirill.fudosin@gmail.com>
2022-11-05 22:07:52 +0300

    Field is created as a grid with numbers

--- Field.rb
+++ Field.rb
@@ -5,15 +5,15 @@ class Field
         @@array = [[1,2,3], [4,5,6], [7,8,9]]
     end
 
-    def change_field(sigil)
-
-    end
+    def change_field(sigil); end
 
     def display_field
-        @@array.each do |item|
-            puts item.each { |elem| elem }.join(" ")
+        @@array.each_with_index do |item, index|
+            #puts "-----" if index > 0
+            puts item.each { |elem| elem }.join("_|_")
         end
     end
 
+    def check_winner; end
     
 end
