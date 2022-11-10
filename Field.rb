3d174e6a0c6b5dfdef0f3223a773601c571667be
Kirill <shishkokirill.fudosin@gmail.com>
2022-11-10 05:12:55 +0300

    Add functions for checking rows, columns and diagonals on coincidence of the same sigils; add validation on whether the cell of array is occupied or not when user enters the  sigil

--- Field.rb
+++ Field.rb
@@ -1,19 +1,74 @@
 class Field
     @@array
+    @@winner
 
     def initialize
-        @@array = [[1,2,3], [4,5,6], [7,8,9]]
+      @@array = [[1,2,3], [4,5,6], [7,8,9]]
+      @@winner = false
     end
 
-    def change_field(sigil); end
+
+
+
+
+    def change_field(move, sigil)
+      @@array.each do |arr|
+        arr.each_with_index { |item, index| arr[index] = sigil if item == move }
+      end
+    end
 
     def display_field
-        @@array.each_with_index do |item, index|
-            #puts "-----" if index > 0
-            puts item.each { |elem| elem }.join("_|_")
-        end
+      @@array.each_with_index do |item, index|
+        puts item.each { |elem| elem }.join("_|_")
+      end
     end
 
-    def check_winner; end
+
+
+
+
+
+    def check_rows(sigil1, sigil2)
+      @@array.each { |arr| @@winner = true if (arr.all?(sigil1) || arr.all?(sigil2)) }
+    end
+
+    def check_columns(sigil1, sigil2)
+      @@array.each_with_index do |arr, index|
+        counter = 0
+        arr.each_with_index do |item, ind|  
+          counter += 1 if @@array[ind][index] == @@array[index][index]
+          
+          @@winner = true if counter == 3
+        end
+      end
+    end
+
+    def check_diagonals(sigil1, sigil2)
+      @@array.each_with_index do |arr, index|
+        main = 0
+        arr.each_with_index do |item, ind|  
+          main += 1 if @@array[index][index] == @@array[ind][ind]
+
+          @@winner = true if main == 3
+        end
+      end
+
+      @@winner = true if @@array[0][2] == @@array[2][0] && @@array[0][2] == @@array[1][1]
+    end
+
+
+    def include_move?(move)
+      @@array.flatten.include?(move)
+    end
+
+    def announce_winner; end
+
+    def self.winner
+        @@winner
+    end
     
 end
+
+#field = Field.new()
+#field.display_field
+#field.change_field(3, "o")
