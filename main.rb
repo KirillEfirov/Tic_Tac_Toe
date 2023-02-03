5927c628403d8004c8dd005ca4926635c3fca7a9
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-02-04 01:59:26 +0300

    Add loop to repeat game

--- main.rb
+++ main.rb
@@ -1,3 +1,10 @@
 require_relative 'game.rb'
 
 game
+
+loop do
+  puts "Would you like to play a new game? Press 'y' for yes or 'n' for no."
+  repeat_input = gets.chomp.downcase
+
+  repeat_input == 'y' ? game : break
+end
