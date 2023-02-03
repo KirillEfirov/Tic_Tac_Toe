ef44eee390efa136025fae5a135774849c370ed3
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-02-04 01:55:32 +0300

    Add #initialize method to init players

--- Player.rb
+++ Player.rb
@@ -1,3 +1,11 @@
 class Player
     attr_accessor :name, :sigil, :move
+
+    def initialize
+        print "What's your name? "
+        @name = gets.chomp
+        
+        print "Choose the sigil to play: "
+        @sigil = gets.chomp
+    end
 end
